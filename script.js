119a1a36c80d0e8bc3d01529194084dba9127b9d
Angel Shelton <angelshelton@Angels-MacBook-Air.local>
2024-02-13 20:17:12 -0800

    finished working on JS dog api

--- script.js
+++ script.js
@@ -445,54 +445,6 @@ function togglePopup() {
     }
 }
 
-// Cat API Code
-const x_url = `https://api.thecatapi.com/v1/breeds`;
-const x_api_key = "live_lbUtmVrpjwLAoF4TTJuGlO7uGg9pNeaGaT3DcxOcHtR429cE9cxWVr7GdGiXauI6"
-let xstoredBreeds = []
-
-fetch(x_url,{headers: {
-    'x-api-key': x_api_key
-  }})
-.then((response) => {
- return response.json();
-})
-.then((data) => {
- 
- //filter to only include those with an `image` object
- data = data.filter(img=> img.image?.url!=null)
- 
-    xstoredBreeds = data;
- 
- for (let i = 0; i < xstoredBreeds.length; i++) {
-  const breed = xstoredBreeds[i];
-  let option = document.createElement('option');
-   
-   //skip any breeds that don't have an image
-   if(!breed.image)continue
-   
-  //use the current array index
-  option.value = i;
-  option.innerHTML = `${breed.name}`;
-document.getElementById('breed_selector').appendChild(option);
-  
-  }
- //show the first breed by default
- showBreedImage(0)
-})
-.catch(function(error) {
- console.log(error);
-});
-
-function showBreedImage(index)
-{ 
-document.getElementById("breed_image").src= CatstoredBreeds[index].image.url;
-
-document.getElementById("breed_json").textContent= CatstoredBreeds[index].temperament
-
-
-document.getElementById("wiki_link").href= CatstoredBreeds[index].wikipedia_url
-document.getElementById("wiki_link").innerHTML= CatstoredBreeds[index].wikipedia_url
-}
 
 // Dog API Code // 
 const url = `https://api.thedogapi.com/v1/breeds`;
